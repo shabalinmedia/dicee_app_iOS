db4432e8472e7a1eba6fb65ffa1c0e626720b760
shabalinmedia <94066403+shabalinmedia@users.noreply.github.com>
2022-07-18 19:16:30 +0600

    Update ViewController.swift

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,15 +12,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
-        let diceArray = [UIImage(named: "DiceOne"),
+     let diceArray = [UIImage(named: "DiceOne"),
                          UIImage(named: "DiceTwo"),
                          UIImage(named: "DiceThree"),
                          UIImage(named: "DiceFour"),
                          UIImage(named: "DiceFive"),
                          UIImage(named: "DiceSix")]
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
         
         diceImageView1.image = diceArray[Int.random(in: 0...5)]
         diceImageView2.image = diceArray[Int.random(in: 0...5)]
